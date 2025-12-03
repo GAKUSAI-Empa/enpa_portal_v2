c434dcebf156c748be69b4ca89792b0ba16f934b
QuachDieuKhanh <quachdieukhanh@gmail.com>
2025-12-03 16:12:18 +0900

    my page

--- src/app/(main)/account/payment_info/page.tsx
+++ src/app/(main)/account/payment_info/page.tsx
@@ -1,9 +1,17 @@
 'use client';
 
 import { Card, CardContent, CardHeader } from '@/component/common/Card';
+import LoadingData from '@/component/common/LoadingData';
 import PageTitle from '@/component/common/PageTitle';
+import useAccountSubcriptionPlan from '../api/useAccountSubcriptionPlan';
 
 const page = () => {
+  const { data, error, isLoading, mutate } = useAccountSubcriptionPlan();
+
+  if (isLoading) {
+    return <LoadingData />;
+  }
+
   return (
     <Card>
       <CardHeader title="エンパタウン株式会社 様" />
@@ -14,7 +22,7 @@ const page = () => {
             <span className="font-medium text-gray-700">プラン名</span>
           </div>
           <div className="md:col-span-2 whitespace-pre-line">
-            <span className="text-gray-900">無料プラン</span>
+            <span className="text-gray-900">{data?.plan_name}</span>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3 border-b border-gray-200">
@@ -22,9 +30,7 @@ const page = () => {
             <span className="font-medium text-gray-700">説明</span>
           </div>
           <div className="md:col-span-2 whitespace-pre-line">
-            <span className="text-gray-900">
-              コンサルティング契約をしているお客様の無料プランです。
-            </span>
+            <span className="text-gray-900">{data?.description}</span>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-3 border-b border-gray-200">
@@ -32,7 +38,17 @@ const page = () => {
             <span className="font-medium text-gray-700">ステータス</span>
           </div>
           <div className="md:col-span-2 whitespace-pre-line">
-            <span className="text-gray-900">有効</span>
+            <span className="text-gray-900">
+              {data?.status == 'active' ? (
+                '有効'
+              ) : (
+                <>
+                  有効なサブスクリプションがありません。
+                  <br />
+                  info@empowerment-town.comまでお問合せください。
+                </>
+              )}
+            </span>
           </div>
         </div>
       </CardContent>
